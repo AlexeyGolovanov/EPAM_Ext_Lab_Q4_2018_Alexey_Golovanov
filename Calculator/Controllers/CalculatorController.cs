294dc0db673ed692ea5ded03054ad03bc0e45de1
AlexeyGolovanov <mryasenpen@gmail.com>
2018-11-21 23:51:05 +0400

    Checking Git

---
+++ Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,63 @@
+﻿using Calculator.Helpers;
+using Calculator.Models;
+using Calculator.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        private const int XDefault = 20;
+        private const int YDefault = 18;
+        private const string ResDefault = "empty";
+
+        public static List<string> Results = new List<string>();
+
+        public ActionResult Add()
+        {
+            return View(new CalcModel() { X = XDefault, Y = YDefault, Result = ResDefault });
+        }
+        [HttpPost]
+        public ActionResult Add(CalcModel model)
+        {
+            var result = "";
+
+            switch (model.Op)
+            {
+                case Operation.Add:
+                    result = (model.X + model.Y).ToString();
+                    break;
+                case Operation.Prod:
+                    result = (model.X * model.Y).ToString();
+                    break;
+                case Operation.Substract:
+                    result = (model.X - model.Y).ToString();
+                    break;
+                case Operation.Div:
+                    result = (model.Y == 0) ? "error! division by zero" : (Math.Round((double)model.X / model.Y, 3)).ToString();
+                    break;
+                case Operation.Eq:
+                    result = (model.X == model.Y).ToString();
+                    break;
+                case Operation.CompB:
+                    result = (model.X > model.Y).ToString();
+                    break;
+                case Operation.CompM:
+                    result = (model.X < model.Y).ToString();
+                    break;
+                case Operation.Pow:
+                    result = Math.Pow(model.X, model.Y).ToString();
+                    break;
+            }
+            model.Result = string.Format("{4:d MMMM H:mm} {0}{2}{1} = {3}", model.X, model.Y, model.Op.DisplayName(), result, DateTime.Now);
+
+            Results.Add(model.Result);
+
+            return View(model);
+        }
+    }
+}
---
+++ Calculator/Models/Operation.cs
@@ -0,0 +1,26 @@
+﻿using Calculator.Resources;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Calculator.Models
+{
+    public enum Operation
+    {
+        [Display(Name = "Add", ResourceType = typeof(CalcResources))]
+        Add,
+        [Display(Name = "Prod", ResourceType = typeof(CalcResources))]
+        Prod,
+        [Display(Name = "Substract", ResourceType = typeof(CalcResources))]
+        Substract,
+        [Display(Name = "Divide", ResourceType = typeof(CalcResources))]
+        Div,
+        [Display(Name = "Equal", ResourceType = typeof(CalcResources))]
+        Eq,
+        [Display(Name = "CompB", ResourceType = typeof(CalcResources))]
+        CompB,
+        [Display(Name = "CompM", ResourceType = typeof(CalcResources))]
+        CompM,
+        [Display(Name = "Pow", ResourceType = typeof(CalcResources))]
+        Pow
+    }
+}
